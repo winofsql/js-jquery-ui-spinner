61dd138f0a094ebf2ebb91d786a2f68714ce86ea
sworc <lightbox@nifty.com>
2024-05-05 20:09:29 +0900

    Update spinner.php

--- spinner.php
+++ spinner.php
@@ -2,6 +2,31 @@
 
 $bootstrap_margin = "m-3";
 
+$ui_type = "black-tie";
+#$ui_type = "smoothness";
+#$ui_type = "ui-lightness";
+#$ui_type = "ui-darkness";
+#$ui_type = "start";
+#$ui_type = "redmond";
+#$ui_type = "sunny";
+#$ui_type = "overcast";
+#$ui_type = "le-frog";
+#$ui_type = "flick";
+#$ui_type = "pepper-grinder";
+#$ui_type = "eggplant";
+#$ui_type = "dark-hive";
+#$ui_type = "cupertino";
+#$ui_type = "south-street";
+#$ui_type = "blitzer";
+#$ui_type = "humanity";
+#$ui_type = "hot-sneaks";
+#$ui_type = "excite-bike";
+#$ui_type = "vader";
+#$ui_type = "dot-luv";
+#$ui_type = "mint-choc";
+#$ui_type = "trontastic";
+#$ui_type = "swanky-purse";
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -9,7 +34,7 @@ $bootstrap_margin = "m-3";
 <title>jQuery BUild Input</title>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.13.2/themes/smoothness/jquery-ui.css">
+<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.13.2/themes/<?= $ui_type ?>/jquery-ui.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js"></script>
 
 <script>
